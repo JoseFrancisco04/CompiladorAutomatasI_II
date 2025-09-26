36eb8f6079e77a486c2c9c4f4a3f218fff7efe0f
Jose Francisco Hernandez Fernando <onefran2004@gmail.com>
2025-09-25 20:38:57 -0600

    Agregar archivos de proyecto.

---
+++ CompiladorAutomatasI_II/ManejadorTokens.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiladorAutomatasI_II
+{
+    public class ManejadorTokens
+    {
+        private List<Token> tokens;
+
+        public ManejadorTokens()
+        {
+            tokens = new List<Token>();
+        }
+
+        public List <string> separarTokens(string codigo)
+        {
+           List<string> lexemas = new List<string>();
+            if (string.IsNullOrEmpty(codigo))
+            {
+                return lexemas;
+
+            }
+            string tokenActual = "";
+            for (int i = 0; i <codigo.Length; i++)
+            {
+                char c = codigo[i];
+                //si hay un espacio, tabulador o salto de linea
+                if (char.IsWhiteSpace(c))
+                {
+                    if (!string.IsNullOrEmpty(tokenActual))
+                    {
+                        lexemas.Add(tokenActual);
+                        tokenActual = "";
+
+                    }
+
+                }
+                else if (c == ';')//si es ;
+                {
+                    if (!string.IsNullOrEmpty(tokenActual))
+                    {
+                        lexemas.Add(tokenActual);
+                        tokenActual = "";
+
+                    }
+                    lexemas.Add(";");
+
+                }
+                else
+                {
+                    tokenActual += c;
+                }
+            }
+
+            //procesar el ultimo token si existe
+            if (!string.IsNullOrEmpty(tokenActual))
+            {
+                lexemas.Add(tokenActual);
+
+            }
+            return lexemas;
+
+        }
+        public void GuardarTokens(List<string> lexemas)
+        {
+            
+
+        }
+
+        //metodo para proecesar codigo fuente y combinar separar y guardar tokens
+        public void ProcesarCodigo(string codigo)
+        {
+            List<string> lexemas = separarTokens(codigo);
+            GuardarTokens(lexemas);
+        }
+
+        //metodo para limpiar la lista de tokens
+        public void LimpiarTokens()
+        {
+            tokens.Clear();
+        }
+    }
+}
---
+++ CompiladorAutomatasI_II/Token.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompiladorAutomatasI_II
+{
+    public enum TipoToken
+    {
+        NUM,
+        PUNTO_Y_COMA,
+        INICIO,
+        FIN,
+        SUMAR,
+        RESTAR,
+        MULTIPLICAR,
+        DIVIDIR,
+        Y,
+        ERROR
+    }
+
+    public class Token
+    {
+        public TipoToken Tipo { get; set; }
+        public string Lexema { get; set; }
+        public string Patron { get; set; }
+
+        public Token(TipoToken tipo, string lexema, string patron)
+        {
+            Tipo = tipo;
+            Lexema = lexema;
+            Patron = patron;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Tipo: {Tipo},Lexema: '{Lexema}', Patrón: '{Patron}'";
+        }
+    }
+}
